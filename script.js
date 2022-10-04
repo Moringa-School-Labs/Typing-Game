17d72dd3f7dd48ce123b9742bdfe78f2f20668ee
catherine <vuthi.esther@gmail.com>
2022-10-04 22:40:16 +0300

    adds typing game files

---
+++ script.js
@@ -0,0 +1,29 @@
+const quote = document.getElementById("quote");
+const input = document.getElementById("typed-value");
+const start = document.getElementById("start");
+
+let wordQueue;
+
+function startGame() {
+  console.log("Game started!");
+
+  let quoteText = "type me";
+  wordQueue = quoteText.split(" ");
+  quote.innerHTML = wordQueue.map((word) => (`<span>${word}</span>`)).join("");
+
+}
+
+function checkInput() {
+  const currentWord = wordQueue[0];
+  const typedValue = input.value.trim();
+
+  if (currentWord !== typedValue) {
+    input.className = currentWord.startsWith(typedValue) ? "" : "error";
+    return;
+  }
+
+  input.value = "";
+}
+
+start.addEventListener("click", startGame);
+input.addEventListener("input", checkInput);
