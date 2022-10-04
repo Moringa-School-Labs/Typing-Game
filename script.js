f0d3d1aed2a8f279f8701d442aaa96a0df2cb5c6
catherine <vuthi.esther@gmail.com>
2022-10-04 23:31:39 +0300

    adds rocket image

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function startGame() {
   let quoteText = "type me";
   wordQueue = quoteText.split(" ");
   quote.innerHTML = wordQueue.map((word) => (`<span>${word}</span>`)).join("");
-
+  startTime = new Date().getTime();
 }
 
 function checkInput() {
